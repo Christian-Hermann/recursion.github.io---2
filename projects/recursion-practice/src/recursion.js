22972df16da2d430e87770456d28d98dbb95a046
Christian-Hermann <christianghermann@gmail.com>
2025-02-14 13:40:34 -0600

    palindrome work

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -161,6 +161,11 @@ return reverse(string.slice(1)) + string[0]
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  string = string.replace(/\s+/g, '').toLowerCase()
+
+  if(string.length <= 0){
+    return true
+  }
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
