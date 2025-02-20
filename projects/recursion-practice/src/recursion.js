89051c87a500b132ac9656c9fc25d9794fe0cfc0
Christian-Hermann <christianghermann@gmail.com>
2025-02-20 11:52:18 -0600

    work

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -355,21 +355,19 @@ var fibonacci = function(n) {
 // nthFibo(5); // 5
 // nthFibo(7); // 13
 // nthFibo(3); // 2
-var nthFibo = function(n, output = [0, 1]) {
+var nthFibo = function(n, fib = [0, 1]) {
 
-if(n < 0){
+if (n < 0){
   return null
 }
-
-if(n === 0){
-  return 0
-}
-if(n === 1){
-  return 1
+if (fib.length > n){
+  return fib[n]
 }
 
-return nthFibo(n - 1)
+fib.push(fib[fib.length - 1] + fib[fib.length - 2]);
 
+
+return nthFibo(n, fib)
 };
 
 // 26. Given an array of words, return a new array containing each word capitalized.
